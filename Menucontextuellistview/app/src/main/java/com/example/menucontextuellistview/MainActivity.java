57f2731ee77778d74f44f04b16bd55043a786ee9
Amira Haouet <haouetamira@gmail.com>
2021-11-29 22:34:08 +0100

    menus commit

--- Menucontextuellistview/app/src/main/java/com/example/menucontextuellistview/MainActivity.java
+++ Menucontextuellistview/app/src/main/java/com/example/menucontextuellistview/MainActivity.java
@@ -12,33 +12,36 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private
-    ListView listView;
-    String[] list={"aaa","bb","ccc"};
-
+    ArrayList<String> contactList = new ArrayList<String>(
+            Arrays.asList("ali ben salah", "amira haouet", "fuoulen ben foulen", "xxx xx ")
+    );
+    ListView contactListview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView=findViewById(R.id.list);
 
-        ArrayAdapter myAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.activity_list_item,list);
-        listView.setAdapter(myAdapter);
+        // ----------------------- Simple Array Adapter ----------------------- //
+        contactListview = findViewById(R.id.contact);
+        registerForContextMenu(contactListview);
 
-        registerForContextMenu(listView);
-
+        ArrayAdapter myAdapter = new ArrayAdapter(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                contactList
+        );
+        contactListview.setAdapter(myAdapter);
 
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
         getMenuInflater().inflate(R.menu.menu, menu);
-        super.onCreateContextMenu(menu, v, menuInfo);
     }
 }
