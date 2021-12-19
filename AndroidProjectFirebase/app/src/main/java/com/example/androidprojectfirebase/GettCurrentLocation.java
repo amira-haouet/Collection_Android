03345bca8a7525afc45e53a62483656c242f02b7
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-19 11:49:17 +0100

    app activity

---
+++ AndroidProjectFirebase/app/src/main/java/com/example/androidprojectfirebase/GettCurrentLocation.java
@@ -0,0 +1,14 @@
+package com.example.androidprojectfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GettCurrentLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gett_current_location);
+    }
+}
