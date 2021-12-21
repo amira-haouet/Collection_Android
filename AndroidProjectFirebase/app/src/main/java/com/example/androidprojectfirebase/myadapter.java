4f67eba51e840445974c2e385079ba4923615312
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-21 15:01:32 +0100

    update project

--- AndroidProjectFirebase/app/src/main/java/com/example/androidprojectfirebase/myadapter.java
+++ AndroidProjectFirebase/app/src/main/java/com/example/androidprojectfirebase/myadapter.java
@@ -66,17 +66,51 @@ import java.util.logging.Handler;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewholder>
-{
-    public myadapter(@NonNull FirebaseRecyclerOptions<model> options)
-    {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.DialogPlusBuilder;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Handler;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myviewholder> {
+    public myadapter(@NonNull FirebaseRecyclerOptions<model> options) {
         super(options);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull final myviewholder holder, final int position, @NonNull final model model)
-    {
+    protected void onBindViewHolder(@NonNull final myviewholder holder, final int position, @NonNull final model model) {
         holder.name.setText(model.getName());
         holder.course.setText(model.getCourse());
         holder.email.setText(model.getEmail());
@@ -85,17 +119,17 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
         holder.edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final DialogPlus dialogPlus=DialogPlus.newDialog(holder.img.getContext())
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
                         .setContentHolder(new ViewHolder(R.layout.dialogcontent))
-                        .setExpanded(true,1100)
+                        .setExpanded(true, 1100)
                         .create();
 
-                View myview=dialogPlus.getHolderView();
-                final EditText purl=myview.findViewById(R.id.uimgurl);
-                final EditText name=myview.findViewById(R.id.uname);
-                final EditText course=myview.findViewById(R.id.ucourse);
-                final EditText email=myview.findViewById(R.id.uemail);
-                Button submit=myview.findViewById(R.id.usubmit);
+                View myview = dialogPlus.getHolderView();
+                final EditText purl = myview.findViewById(R.id.uimgurl);
+                final EditText name = myview.findViewById(R.id.uname);
+                final EditText course = myview.findViewById(R.id.ucourse);
+                final EditText email = myview.findViewById(R.id.uemail);
+                Button submit = myview.findViewById(R.id.usubmit);
 
                 purl.setText(model.getPurl());
                 name.setText(model.getName());
@@ -107,11 +141,11 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
                 submit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Map<String,Object> map=new HashMap<>();
-                        map.put("purl",purl.getText().toString());
-                        map.put("name",name.getText().toString());
-                        map.put("email",email.getText().toString());
-                        map.put("course",course.getText().toString());
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("purl", purl.getText().toString());
+                        map.put("name", name.getText().toString());
+                        map.put("email", email.getText().toString());
+                        map.put("course", course.getText().toString());
 
                         FirebaseDatabase.getInstance().getReference().child("students")
                                 .child(getRef(position).getKey()).updateChildren(map)
@@ -138,7 +172,7 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
         holder.delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder=new AlertDialog.Builder(holder.img.getContext());
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.img.getContext());
                 builder.setTitle("Delete Panel");
                 builder.setMessage("Delete...?");
 
@@ -165,28 +199,26 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
 
     @NonNull
     @Override
-    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerow,parent,false);
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerow, parent, false);
         return new myviewholder(view);
     }
 
 
-    class myviewholder extends RecyclerView.ViewHolder
-    {
+    class myviewholder extends RecyclerView.ViewHolder {
         CircleImageView img;
-        ImageView edit,delete;
-        TextView name,course,email;
-        public myviewholder(@NonNull View itemView)
-        {
-            super(itemView);
-            img=(CircleImageView) itemView.findViewById(R.id.img1);
-            name=(TextView)itemView.findViewById(R.id.nametext);
-            course=(TextView)itemView.findViewById(R.id.coursetext);
-            email=(TextView)itemView.findViewById(R.id.emailtext);
+        ImageView edit, delete;
+        TextView name, course, email;
 
-            edit=(ImageView)itemView.findViewById(R.id.editicon);
-            delete=(ImageView)itemView.findViewById(R.id.deleteicon);
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+            img = (CircleImageView) itemView.findViewById(R.id.img1);
+            name = (TextView) itemView.findViewById(R.id.nametext);
+            course = (TextView) itemView.findViewById(R.id.coursetext);
+            email = (TextView) itemView.findViewById(R.id.emailtext);
+
+            edit = (ImageView) itemView.findViewById(R.id.edit);
+            delete = (ImageView) itemView.findViewById(R.id.delete);
         }
     }
 }
